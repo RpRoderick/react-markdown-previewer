8f117eb8c7a0df44b46e81955c32e3a2af9b0dcd
Ryan Roderick <ryan@designmonster.com>
2020-08-31 13:34:26 -0400

    better boilerplate

--- src/components/structural/home/Home.jsx
+++ src/components/structural/home/Home.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import styles from './Home.module.scss';
+import MarkdownEditor from '../markdownEditor/MarkdownEditor';
 
-class Home extends React.Component {
-    render() {
-        return (
-            <div className={styles.home}>
-                You are seeing the Home Component
-            </div>
-        );
-    }
+function Home() {
+  return (
+    <div className={styles.home}>
+      <MarkdownEditor />
+    </div>
+  );
 }
 
-export default Home
+export default Home;
---
+++ src/components/structural/markdownEditor/MarkdownEditor.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './MarkdownEditor.module.scss';
+
+function MarkdownEditor() {
+  return (
+    <div className={styles.home}>
+      Hello You are in the Markdown Editor
+    </div>
+  );
+}
+
+export default MarkdownEditor;
