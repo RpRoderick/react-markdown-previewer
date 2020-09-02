74327598100bb51643dcfca9ecf3bd56bc8d6ce1
Ryan Roderick <ryan@designmonster.com>
2020-09-01 20:24:44 -0400

    styling is done
    styling is done.  media queries added.

--- src/components/structural/previewArea/PreviewArea.jsx
+++ src/components/structural/previewArea/PreviewArea.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-// import styles from 'components/structural/previewArea/PreviewArea.module.scss';
+import styles from 'components/structural/previewArea/PreviewArea.module.scss';
 import marked from 'marked';
 import { PropTypes } from 'prop-types';
 
 function PreviewArea(props) {
     return (
-      <div className="preview_area_container">
-        <div id="preview"
+      <div className={styles.preview_area_container}>
+        <div className={styles.preview}
           dangerouslySetInnerHTML={{
             __html: marked(props.markdown)
           }}>
