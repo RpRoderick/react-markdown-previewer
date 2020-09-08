615f657a673eb8cade9f56ec44f4b0df616fc951
Ryan Roderick <ryan@designmonster.com>
2020-09-08 16:32:42 -0400

    Updates

--- precache-manifest.e6bd619c84f809ece7696842c1ce8344.js
+++ precache-manifest.e6bd619c84f809ece7696842c1ce8344.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b39cc239ed3f9e4f18081e4dd186d2e4",
+    "revision": "acdb75d7514dee54bf6f0df6a2ee397d",
     "url": "/react-markdown-previewer/index.html"
   },
   {
-    "revision": "50b4c7161dc20dbe1492",
-    "url": "/react-markdown-previewer/static/css/main.2ea045a6.chunk.css"
+    "revision": "f622c275044c1cbe9093",
+    "url": "/react-markdown-previewer/static/css/main.3a166d27.chunk.css"
   },
   {
     "revision": "184d42f2bba6db180662",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-markdown-previewer/static/js/2.cdb0ae96.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "50b4c7161dc20dbe1492",
+    "revision": "f622c275044c1cbe9093",
     "url": "/react-markdown-previewer/static/js/main.6766ef09.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-markdown-previewer/precache-manifest.97d4591aded994ac143b133063dd7cb0.js"
+  "/react-markdown-previewer/precache-manifest.e6bd619c84f809ece7696842c1ce8344.js"
 );
 
 self.addEventListener('message', (event) => {
