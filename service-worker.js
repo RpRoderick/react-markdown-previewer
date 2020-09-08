57628a648fb9fbf3f2b08e88add8b8243315f3c0
Ryan Roderick <ryan@designmonster.com>
2020-09-08 14:41:09 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-markdown-previewer/precache-manifest.e28b853f34e758f4f23d8bb300dcc00c.js"
+  "/react-markdown-previewer/precache-manifest.e582f64a83d4c9073256469cb05c081f.js"
 );
 
 self.addEventListener('message', (event) => {
